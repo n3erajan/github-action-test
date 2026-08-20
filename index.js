f554ec29fdc11ff49d55fa243ffcfa4238850a89
neerajann <neerajan.vfx@gmail.com>
2026-08-20 13:07:35 +0545

    Test for  v2 auto deployment

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json('Welcome! this it test v1!')
+  res.json('Welcome! this it test v2!')
+})
+
+app.get('/*splat', (req, res) => {
+  res.send(`<h1> Your IP address is: ${req.ip}</h1> `)
 })
 
 app.listen(3000, () => {
